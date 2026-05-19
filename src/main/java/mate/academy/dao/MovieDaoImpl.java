55ad21febf60274829459af42d1a9223fba6f534
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2026-05-19 12:43:16 +0200

    hibernate-configuration

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -6,7 +6,6 @@ import mate.academy.lib.Dao;
 import mate.academy.model.Movie;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 @Dao
